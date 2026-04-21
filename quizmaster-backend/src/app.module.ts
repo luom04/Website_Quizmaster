0e9ee434a6d431e9da8c0f78d42acff19aea1517
Nguyen Van Luom <luom.py.tuyan.phulong@gmail.com>
2026-04-21 11:14:53 +0700

    restructure project and reset prisma schema

--- quizmaster-backend/src/app.module.ts
+++ quizmaster-backend/src/app.module.ts
@@ -1,6 +1,28 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './modules/auth/auth.module';
+import { UsersModule } from './modules/users/users.module';
+import { CategoriesModule } from './modules/categories/categories.module';
+import { QuestionsModule } from './modules/questions/questions.module';
+import { QuizzesModule } from './modules/quizzes/quizzes.module';
+import { AttemptsModule } from './modules/attempts/attempts.module';
+import { AdminModule } from './modules/admin/admin.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PrismaModule,
+    AuthModule,
+    UsersModule,
+    CategoriesModule,
+    QuestionsModule,
+    QuizzesModule,
+    AttemptsModule,
+    AdminModule,
+  ],
   controllers: [],
   providers: [],
 })
